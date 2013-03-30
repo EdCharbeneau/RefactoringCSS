8fba6f46079d033ca8d454151b3ed72dc9a066dc
Ed Charbeneau <ed.charbeneau@gmail.com>
2013-03-30 16:56:26 -0400

    Changed CSS output directory to 'Content'

--- RefactoringCSS/RefactoringCSS/config.rb
+++ RefactoringCSS/RefactoringCSS/config.rb
@@ -2,7 +2,8 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+#css_dir = "stylesheets" #Default
+css_dir = "Content" #MVC convention
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
